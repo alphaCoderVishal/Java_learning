fb45293b70094c0d666434dd961ab069878015c9
vishal5677 <61550225+vishal5677@users.noreply.github.com>
2022-02-12 23:17:23 +0530

    this is interest rate calculator

---
+++ Interest_Rate_Calculator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Pkg_Self_Learning;
+
+/**
+ * @author vishal567795
+ *
+ */
+public class Interest_Rate_Calculator {
+
+	/**
+	 * 
+	 */
+	public Interest_Rate_Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
